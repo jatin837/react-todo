a16b65876059a0b0f953aa1e593b4432c5de24ac
dj4t9n <pandeyjatin47@gmail.com>
2021-08-13 00:27:50 +0530

    name property added to Todo component

--- src/App.js
+++ src/App.js
@@ -61,8 +61,8 @@ function App(props) {
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading"
       >
-				<Todo />
-				<Todo />
+				<Todo name="Solve Differential Equation"/>
+				<Todo name="Watch Rick and Morty"/>
 
       </ul>
     </div>
