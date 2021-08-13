cce52215576f99d7255261feafadd4544153730b
dj4t9n <pandeyjatin47@gmail.com>
2021-08-14 01:52:16 +0530

    implemented adding task to tasklist and rendering
    TODO
    
    install nanoid to have unique key literals

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
-
-const addTask = (name) => {
-	alert(name);
-}
+import React, {useState} from "react";
 
 function App(props) {
-	const taskList = props.tasks.map(task => 
+
+	const initialTaskList = props.tasks.map(task => 
 		(<Todo name={task.name} completed={task.completed} id={task.id} key={task.id}/>)
 	);
+
+	const [taskList, setTaskList] = useState(initialTaskList)
+
+	const addTask = (name) => {
+		const newTask = <Todo
+			id = "id" 
+			name = {name}
+			completed = {false}
+		/>
+		setTaskList([...taskList, newTask]);
+	}
 
   return (
     <div className="todoapp stack-large">
