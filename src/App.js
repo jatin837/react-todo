ce1373d2df770b9a2eda1562a3282ca0f8ef60c4
dj4t9n <pandeyjatin47@gmail.com>
2021-08-14 13:21:41 +0530

    edit handler to change an existing task

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import FilterButton from "./components/FilterButton";
 import React, {useState} from "react";
 import { nanoid } from "nanoid";
 
-function App(props) {
+const App = (props) => {
 
 	const [taskList, setTaskList] = useState(props.tasks);
+
+	/*
+	 * PROP CALLBACK
+	 toggleTaskCompleted takes an id of task and 
+	 toggles it's complete property and set the state to the new task list
+	 */
 
 	const toggleTaskCompleted = (id) => {
 		const updatedTasks = taskList.map( (task) => {
@@ -19,6 +25,11 @@ function App(props) {
 		setTaskList(updatedTasks);
 	}
 
+	/*
+	 * PROP CALLBACK
+	 deleteTask takes ID of 'to be deleted' task, filter out tasks which does not have this ID
+	 and update the taskList
+	 */
 	const deleteTask = (id) => {
 		const newTaskList = taskList.filter( 
 			(task) => id !== task.id 
@@ -33,9 +44,11 @@ function App(props) {
 			completed={task.completed} 
 			toggleTaskCompleted={toggleTaskCompleted}
 			deleteTask={deleteTask}
+			editTask={editTask}
 			key={task.id} 
 		/>
 	);
+
 	const addTask = (name) => {
 		const newTask = {
 			id:  "todo-" + nanoid(),
@@ -45,18 +58,34 @@ function App(props) {
 		setTaskList([...taskList, newTask]);
 	};
 
+	const editTask = (id, newName) => {
+		const editedTaskList = taskList.map(
+			(task) => {
+				if (task.id === id)
+					return {...task, name:newName};
+				else 
+					return task;
+			}
+		)
+		setTaskList(editedTaskList);
+	}
+
   return (
     <div className="todoapp stack-large">
       <h1>Todo</h1>
+
 			<Form addTask={addTask}/>
+
       <div className="filters btn-group stack-exception">
 				<FilterButton name="All" ariaStatus={true} />
 				<FilterButton name="Active" ariaStatus={false} />
 				<FilterButton name="Completed" ariaStatus={false} />
 			</div>
+
       <h2 id="list-heading">
 				{taskList.length} tasks remaining
       </h2>
+
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
